ef2346a01331c015d43bc716bd5384f2c5af7a7d
johncross00 <johncross.ktm@gmail.com>
2024-01-18 07:41:54 +0000

    Init

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Messenger',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Whatsapp'),
+    ),
+        body: Text("Welcome to whatsapp, please sign in to continue"),
+    )
+    );
+  }
+}
